44d2ca8990c9dceca71c58dbceb774534b8664fc
Adroaldo <hygino@msn.com>
2022-07-11 10:26:08 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import logo from '../../assets/img/logo.svg';
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/hygino82"> @hygino82</a>
+                </p>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
