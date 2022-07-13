db9a3c69579b203d7a38715fa82cfa83b7ac0d17
Adroaldo <hygino@msn.com>
2022-07-13 09:37:55 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -7,7 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -16,9 +19,18 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
-    @GetMapping
+    @GetMapping("/all")
     public Page<Sale> findAll(Pageable pageable) {
         Page<Sale> page = service.findAll(pageable);
         return page;
+    }
+
+    @GetMapping
+        //na web usa string não LocalDate
+    Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate, pageable);
     }
 }
--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -17,5 +19,15 @@ public class SaleService {
 
     public Page<Sale> findAll(Pageable pageable) {
         return repository.findAll(pageable);
+    }
+
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+        //data com o dia de hoje
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
